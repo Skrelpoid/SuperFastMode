e56830332b93228b3b80d64185ee4ed01b0dd7bf
Dustin <skrelpoid@protonmail.com>
2023-01-14 20:06:32 +0100

    Fix Stupid Typo

--- src/main/java/skrelpoid/superfastmode/patches/LwjglGraphicsPatches.java
+++ src/main/java/skrelpoid/superfastmode/patches/LwjglGraphicsPatches.java
@@ -10,7 +10,7 @@ public class LwjglGraphicsPatches {
 
 	@SpirePatch(clz = com.badlogic.gdx.backends.lwjgl.LwjglGraphics.class, method = "getDeltaTime")
 	// If --imgui is set or StS is started on a steam deck, lwjgl3 is loaded instead
-	@SpirePatch(cls = "com.badlogic.gdx.backends.lwjgl3.LwjglGraphics3", method = "getDeltaTime", optional = true)
+	@SpirePatch(cls = "com.badlogic.gdx.backends.lwjgl3.Lwjgl3Graphics", method = "getDeltaTime", optional = true)
 	public static class DeltaPatch {
 		public static ExprEditor Instrument() {
 			return new ExprEditor() {
